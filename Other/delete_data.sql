66777a6c6c19f716ee1ed67002e8c734f5420b95
jhon0010 <jhoning.soft@gmail.com>
2015-02-07 06:27:25 -0800

    Se crea escript para borrar datos de las tablas, se crean mas inserciones en los datos habilitados

---
+++ Other/delete_data.sql
@@ -0,0 +1,6 @@
+DELETE FROM main_sensor;
+DELETE FROM main_sensor_type;
+DELETE FROM main_state_definition;
+DELETE FROM main_building;
+DELETE FROM main_neighborhood;
+DELETE FROM main_building_type;
