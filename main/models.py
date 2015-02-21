0870d65421748a0be00dd1243eb414a105615faf
jnizita <jnizit@hotmail.com>
2015-02-21 10:29:52 -0500

    Se incluye el main en installed apps al settings y ajustes del modelo

--- main/models.py
+++ main/models.py
@@ -4,31 +4,32 @@ from django.contrib.auth.models import User, Group
 # Create your models here.
 
 class Building_type(models.Model):
-    name=models.CharField(max_length=30, vebose_name='Building type')
+    name=models.CharField(max_length=30, verbose_name='Building type')
+
 
 class Neighborhood(models.Model):
-    name=models.CharField(max_length=30, vebose_name='Neighborhood')
-    risk_level=models.IntegerField(verbose_naem='Risk level')
+    name=models.CharField(max_length=30, verbose_name='Neighborhood')
+    risk_level=models.IntegerField(verbose_name='Risk level')
 
 class Building(models.Model):
-    name = models.CharField(max_length=30, vebose_name='Building name')
-    address = models.CharField(max_length=30, vebose_name='Address')
+    name = models.CharField(max_length=30, verbose_name='Building name')
+    address = models.CharField(max_length=30, verbose_name='Address')
     user_id=models.ForeignKey(User, verbose_name='User')
     building_type=models.ForeignKey(Building_type, verbose_name='Building type')
-    neighborhood_id=models.ForeignKey(Neighborhood,verbose_name='Neighborhood')
+    neighborhood_id=models.ForeignKey(Neighborhood, verbose_name='Neighborhood')
 
-class State_definition(models.model):
-    name = models.CharField(max_length=30,verbose_name='State definition name')
-    value= models.BooleanField(verbose_name='State definition value')
+class State_definition(models.Model):
+    name = models.CharField(max_length=30, verbose_name='State definition name')
+    value= models.BooleanField( verbose_name='State definition value')
 
-class Sensor_type(models.model):
-    name=models.CharField(max_length=30,verbose_name='Sensor type name')
+class Sensor_type(models.Model):
+    name=models.CharField(max_length=30, verbose_name='Sensor type name')
     State_definition_id=models.ForeignKey(State_definition, verbose_name='Sensor type state definition')
 
-class Sensor(models.model):
-    name=models.CharField(max_length=50,verbose_name='Sensor name')
+class Sensor(models.Model):
+    name=models.CharField(max_length=50, verbose_name='Sensor name')
     description=models.CharField(max_length=100, verbose_name='Sensor description')
-    state=models.BooleanField(verbose_name='Sensor state')
-    criticaly=models.ImageField(verbose_name='Sensor criticaly')
-    Building_id=models.ForeignKey(Building,verbose_name='Building')
-    Sensor_type_id=models.ForeignKey(Sensor_type,verbose_name='Sensor type')
+    state=models.BooleanField( verbose_name='Sensor state')
+    criticaly=models.ImageField( verbose_name='Sensor criticaly')
+    Building_id=models.ForeignKey(Building, verbose_name='Building')
+    Sensor_type_id=models.ForeignKey(Sensor_type, verbose_name='Sensor type')
