ea0a89444fc2a0976c56079b173c064f034b8b51
danesco <danesco0507@gmail.com>
2015-03-08 16:12:34 -0700

    ajustes para servir estaticos en produccion

--- SmartHome/urls.py
+++ SmartHome/urls.py
@@ -15,3 +15,7 @@ urlpatterns = patterns('',
 
     #url(r'^', 'django.contrib.auth.views.login', {'template_name':'login.html'}),
 )
+
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
